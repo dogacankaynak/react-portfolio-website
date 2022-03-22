5c60ca2f19adb5eab36ac53929cf890ea2919896
Doğacan Kaynak <36055566+dogacankaynak@users.noreply.github.com>
2022-03-22 21:17:17 +0300

    creating files

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+/*#rafce In js extension using image is going to be difficult thats why I'm creating this one*/
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>App</div>
+  )
+}
+
+export default App
