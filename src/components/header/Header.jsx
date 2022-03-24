918389eb5428b66a43b9c963c68f34e71e34b534
Doğacan Kaynak <36055566+dogacankaynak@users.noreply.github.com>
2022-03-25 02:01:17 +0300

    header component(needs fix on socials & picture)

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,9 +1,25 @@
 import React from 'react'
 import './header.css'
+import CTA from './CTA.jsx'
+import ME from '../../assets/me2.png'
+import HeaderSocial from './HeaderSocials'
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <header>
+      <div className="container header__container">
+        <h5>Hello I'm</h5>
+        <h1>Dogacan Kaynak</h1>
+        <h5 classname ="text-light">Fullstack Developer</h5>
+        <CTA/>
+        <HeaderSocial/>
+        <div className="me">
+          <img src={ME} alt="me"/>
+        </div>
+
+        <a href="#contact" className='scroll__down'>Scroll Down</a>
+      </div>
+    </header>
   )
 }
 
