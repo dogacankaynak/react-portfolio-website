fc0955f3b2934dab57d93fbb1dd68bbd2d01a89f
Doğacan Kaynak <36055566+dogacankaynak@users.noreply.github.com>
2022-04-07 01:08:18 +0300

    contacts(without emailjs)

--- src/components/contacts/Contacts.jsx
+++ src/components/contacts/Contacts.jsx
@@ -1,9 +1,44 @@
 import React from 'react'
 import './contacts.css'
+import {MdOutlineEmail} from 'react-icons/md'
+import {RiMessengerLine} from 'react-icons/ri'
+import {BsWhatsapp} from 'react-icons/bs'
 
 const Contact = () => {
   return (
-    <section id='contact'>Contact</section>
+    <section id='contact'>
+      <h5>Get In Touch</h5>Contact
+      <h2>Contact Me</h2>
+
+      <div className="container contact__container">
+        <div className="contact__options">
+          <article className="contact__option">
+            <MdOutlineEmail className='contact__option-icon'/>
+            <h4>Email</h4>
+            <a href="mailto:kaynakdogacan@gmail.com" target="_blank">Send an email</a>
+          </article>
+          <article className="contact__option">
+            <RiMessengerLine className='contact__option-icon'/>
+            <h4>Messenger</h4>
+            <a href="https://m.me/dogacankaynak" target="_blank">Send a message</a>
+          </article>
+          <article className="contact__option">
+            <BsWhatsapp className='contact__option-icon'/>
+            <h4>WhatsApp</h4>
+            <a href="https://api.whatsapp.com/send?phone=+905394772999" target="_blank">Send a message</a>
+          </article>
+        </div>
+        {/* End of Contact */}
+        <from action ="">
+          <input type ="text" name ='name' placeholder='Full Name' required/>
+          <input type ="email" name ='email' placeholder='Email' required/>
+          <textarea type ="message" rows = "10" placeholder='Message' required></textarea>
+          <button type='submit' className='btn btn-primary'>Send Message</button>
+        </from>
+      </div>
+
+
+      </section>
   )
 }
 
